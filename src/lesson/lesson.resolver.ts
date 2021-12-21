14b955fc8d20cd21c72889775783d3fed00649ed
Rakesh D R <rakeshdr543@gmail.com>
2021-12-21 12:55:41 +0530

    Added lesson module

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,11 +1,18 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Lesson } from './lesson.entity';
+import { CreateLessonInput } from './lesson.input';
 import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
   constructor(private lessonService: LessonService) {}
+
+  @Query((returns) => [LessonType])
+  lessons() {
+    return this.lessonService.getAllLessons();
+  }
+
   @Query((returns) => LessonType)
   lesson(@Args('id') id: string): Promise<Lesson> {
     return this.lessonService.getSingleLesson(id);
@@ -13,10 +20,8 @@ export class LessonResolver {
 
   @Mutation((returns) => LessonType)
   createLesson(
-    @Args('name') name: string,
-    @Args('startDate') startDate: string,
-    @Args('endDate') endDate: string,
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
   ): Promise<Lesson> {
-    return this.lessonService.createLesson(name, startDate, endDate);
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 import { Lesson } from './lesson.entity';
+import { CreateLessonInput } from './lesson.input';
 
 @Injectable()
 export class LessonService {
@@ -10,7 +11,8 @@ export class LessonService {
     @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
   ) {}
 
-  async createLesson(name, startDate, endDate): Promise<Lesson> {
+  async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate } = createLessonInput;
     const lesson = this.lessonRepository.create({
       id: uuid(),
       name,
@@ -18,6 +20,10 @@ export class LessonService {
       endDate,
     });
     return this.lessonRepository.save(lesson);
+  }
+
+  async getAllLessons(): Promise<Lesson[]> {
+    return this.lessonRepository.find({});
   }
 
   async getSingleLesson(id: string): Promise<Lesson> {
