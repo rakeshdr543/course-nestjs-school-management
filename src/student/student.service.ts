63908b643e635c8ec6a4abce4609f270083dca17
Rakesh D R <rakeshdr543@gmail.com>
2021-12-21 16:35:22 +0530

    Added student resolver

---
+++ src/student/student.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { CreateStudentInput } from './create-student.input';
+import { Student } from './student.entity';
+
+@Injectable()
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+    await this.studentRepository.save(student);
+    return student;
+  }
+
+  async getAllStudents(): Promise<Student[]> {
+    const students = await this.studentRepository.find();
+    return students;
+  }
+
+  async getSingleStudent(id: string): Promise<Student> {
+    const student = await this.studentRepository.findOne({ id });
+    return student;
+  }
+}
