2d820991912bf373a30dfc561a27407bfd883e05
Rakesh D R <rakeshdr543@gmail.com>
2021-12-21 12:25:55 +0530

    Added lesson resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb+srv://root:root@cluster0.oxvs2.mongodb.net/school?retryWrites=true&w=majority',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,22 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Lesson } from './lesson.entity';
+import { LessonService } from './lesson.service';
+import { LessonType } from './lesson.type';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+  @Query((returns) => LessonType)
+  lesson(@Args('id') id: string): Promise<Lesson> {
+    return this.lessonService.getSingleLesson(id);
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ): Promise<Lesson> {
+    return this.lessonService.createLesson(name, startDate, endDate);
+  }
+}
